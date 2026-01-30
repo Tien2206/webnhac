c60ddd08e3b6b0d762ecab99ab082e08652c77b9
Tien2206 <vungoctien.k15@stu.upt.edu.vn>
2026-01-30 16:10:59 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserApp from './user/UserApp';
 import AdminDashboard from './admin/page/AdminDashboard';
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserApp />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="*" element={<UserApp />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
